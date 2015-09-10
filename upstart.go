33ac54341bf83cf33520822ab00cf67312abea6b
Akshay Moghe <akshay@defend7.com>
2015-09-09 21:37:21 -0700

    Bail all funcs if hasUpstart is false.
    Initialize hasUpstart at pkg init using Detect().

--- upstart.go
+++ upstart.go
@@ -2,7 +2,19 @@
 // Note that this is only usable on systems where upstart is the init system.
 package upstart
 
-import "github.com/godbus/dbus"
+import (
+	"fmt"
+
+	"github.com/godbus/dbus"
+)
+
+var (
+	hasUpstart = false
+)
+
+func init() {
+	hasUpstart = Detect()
+}
 
 // Detect returns true if the system we're running on has an upstart daemon listening on
 // its known address and we're able to communicate with it.
@@ -27,18 +39,30 @@ func Detect() bool {
 
 // StartJob starts the specified job.
 func StartJob(name string) error {
+	if !hasUpstart {
+		return fmt.Errorf("Unable to communicate with upstart")
+	}
+
 	job := &job{Name: name}
 	return job.StartWithEnv(map[string]string{})
 }
 
 // StopJob stops the specified job.
 func StopJob(name string) error {
+	if !hasUpstart {
+		return fmt.Errorf("Unable to communicate with upstart")
+	}
+
 	job := &job{Name: name}
 	return job.Stop()
 }
 
 // RestartJob restarts the specified job.
 func RestartJob(name string) error {
+	if !hasUpstart {
+		return fmt.Errorf("Unable to communicate with upstart")
+	}
+
 	job := &job{Name: name}
 	return job.Restart()
 }
@@ -46,6 +70,10 @@ func RestartJob(name string) error {
 // ListJobInstances lists the instances of the specified job. Unless configured to run
 // as multiple instances, a job has a single instance.
 func ListJobInstances(name string) ([]string, error) {
+	if !hasUpstart {
+		return nil, fmt.Errorf("Unable to communicate with upstart")
+	}
+
 	ret := []string{}
 	job := &job{Name: name}
 
